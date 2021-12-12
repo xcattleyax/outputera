236c7eaf8f4b85edd3038a199e20e8b13e38d96b
xcattleyax <novels.satellite@gmail.com>
2021-12-12 16:20:23 +0900

    アクティブハッシュの導入

--- app/models/diary.rb
+++ app/models/diary.rb
@@ -1,8 +1,8 @@
 class Diary < ApplicationRecord
 
-  with options presence: true do
-    validates :time
-    validates :category_id
-  end
+  belongs_to :category
+
+  validates :time, presence: true
+  validates :category_id, numericality: { other_than: 1 , message: "can't be blank"}
 
 end
