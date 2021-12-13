448a3e52d40696ac3322cdb7e23eacfd0b045ef6
xcattleyax <novels.satellite@gmail.com>
2021-12-13 14:21:54 +0900

    diaryモデル作成

---
+++ db/migrate/20211213051946_create_diaries.rb
@@ -0,0 +1,10 @@
+class CreateDiaries < ActiveRecord::Migration[6.0]
+  def change
+    create_table :diaries do |t|
+      t.integer :time, null: false
+      t.integer :category_id, null: false
+      t.references :user, null: false, foreign_key: true
+      t.timestamps
+    end
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_12_13_033156) do
+ActiveRecord::Schema.define(version: 2021_12_13_051946) do
+
+  create_table "diaries", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.integer "time", null: false
+    t.integer "category_id", null: false
+    t.bigint "user_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_diaries_on_user_id"
+  end
 
   create_table "trends", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.integer "category_id", null: false
@@ -33,5 +42,6 @@ ActiveRecord::Schema.define(version: 2021_12_13_033156) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "diaries", "users"
   add_foreign_key "trends", "users"
 end
