3f09350adbf4a7fb7da7b54168dfc8736d6b2e7a
xcattleyax <novels.satellite@gmail.com>
2021-12-14 13:37:29 +0900

    ボタンの見た目変更

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,5 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'diaries#index'
   resources :diaries, only:[:create]
-  resources :trends, only:[:show, :new, :create, :edit, :destroy]
+  resources :trends, except:[:index, :update]
 end
