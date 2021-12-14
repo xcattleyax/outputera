9b46e7df73d65a3c565f973895d13515444b104d
xcattleyax <novels.satellite@gmail.com>
2021-12-14 14:06:56 +0900

    userモデル単体テスト

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,5 +5,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   has_many :trends
+
+  validates :name, presence: true
   
 end
---
+++ spec/models/user_spec.rb
@@ -0,0 +1,55 @@
+require 'rails_helper'
+
+RSpec.describe User, type: :model do
+  before do
+    @user = FactoryBot.build(:user)
+  end
+
+  describe 'ユーザー新規登録' do
+    context 'ユーザーの新規登録ができる' do
+      it '正しく情報を入力していれば新規登録ができる' do
+        expect(@user).to be_valid
+      end
+    end
+
+    context 'ユーザーの新規登録ができない' do
+      it 'nameが空である' do
+        @user.name = ""
+        @user.valid?
+        expect(@user.errors.full_messages).to include("Name can't be blank")
+      end
+      it 'emailが空である' do
+        @user.email = ""
+        @user.valid?
+        expect(@user.errors.full_messages).to include("Email can't be blank")
+      end
+      it 'emailが不正である' do
+        @user.email = "abcdefg"
+        @user.valid?
+        expect(@user.errors.full_messages).to include("Email is invalid")
+      end
+      it 'emailが既に登録されている' do
+        @user.save
+        another_user = FactoryBot.build(:user)
+        another_user.email = @user.email
+        another_user.valid?
+        expect(another_user.errors.full_messages).to include("Email has already been taken")
+      end
+      it 'passwordが空である' do
+        @user.password = ""
+        @user.valid?
+        expect(@user.errors.full_messages).to include("Password can't be blank")
+      end
+      it 'passwordと確認用のpasswordが一緒でない' do
+        @user.password = "abcd1234"
+        @user.valid?
+        expect(@user.errors.full_messages).to include("Password confirmation doesn't match Password")
+      end
+      it 'passwordが6文字未満である' do
+        @user.password = "abc12"
+        @user.valid?
+        expect(@user.errors.full_messages).to include("Password is too short (minimum is 6 characters)")
+      end
+    end
+  end
+end
