75fc8cdbcc492dc0e8d9d8fb0c537c896e28aba0
xcattleyax <novels.satellite@gmail.com>
2021-12-15 15:12:20 +0900

    presentationモデル作成

---
+++ db/migrate/20211215060917_create_presentations.rb
@@ -0,0 +1,10 @@
+class CreatePresentations < ActiveRecord::Migration[6.0]
+  def change
+    create_table :presentations do |t|
+      t.string :title, null: false
+      t.integer :category_id, null: false
+      t.references :user, null: false, foreign_key: true
+      t.timestamps
+    end
+  end
+end
