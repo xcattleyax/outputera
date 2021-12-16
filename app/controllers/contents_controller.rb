5a8f9be747531640c0bbb2e95dbe3f7a43718c40
xcattleyax <novels.satellite@gmail.com>
2021-12-16 10:22:10 +0900

    プレゼン作成機能の実装

--- app/controllers/contents_controller.rb
+++ app/controllers/contents_controller.rb
@@ -2,5 +2,28 @@ class ContentsController < ApplicationController
 
   def new
     @content = PresentationContent.new
+    @presentation = Presentation.find(params[:presentation_id])
+    @contents = @presentation.presentation_content
+    @page = @contents.pluck(:page)
+    if @page == nil
+      @page = 1
+    else 
+      @page = @page.max.to_i + 1
+    end
+  end
+
+  def create
+    @content = PresentationContent.new(content_params)
+    if @content.save
+      redirect_to new_presentation_content_path(@content.presentation_id)
+    else
+      @page = @content.page
+      render 'new'
+    end
+  end
+
+  private
+  def content_params
+    params.require(:presentation_content).permit(:page, :content, :text).merge(presentation_id: params[:presentation_id])
   end
 end
