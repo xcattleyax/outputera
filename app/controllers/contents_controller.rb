b8477a5656d2569aacfd8ba4d7f03cdaf1020ca4
xcattleyax <novels.satellite@gmail.com>
2021-12-16 14:47:34 +0900

    各種調整

--- app/controllers/contents_controller.rb
+++ app/controllers/contents_controller.rb
@@ -1,8 +1,8 @@
 class ContentsController < ApplicationController
   def new
-    @content = PresentationContent.new
+    @content = Content.new
     @presentation = Presentation.find(params[:presentation_id])
-    @contents = @presentation.presentation_content
+    @contents = @presentation.contents
     @page = @contents.pluck(:page)
     @page = if @page.nil?
               1
@@ -12,7 +12,7 @@ class ContentsController < ApplicationController
   end
 
   def create
-    @content = PresentationContent.new(content_params)
+    @content = Content.new(content_params)
     if @content.save
       redirect_to new_presentation_content_path(@content.presentation_id)
     else
@@ -21,9 +21,23 @@ class ContentsController < ApplicationController
     end
   end
 
-  private
+  def edit
+    @presentation = Presentation.find(params[:presentation_id])
+    @contents = @presentation.contents
+    @page = params[:page]
+    @content = Content.find_by(presentation_id: @presentation.id, page: @page)
+  end
+
+  def update
+    content = Content.find(params[:id])
+    content.update(content_params)
+    redirect_to new_presentation_content_path(content.presentation_id)
+  end
+
+    private
 
   def content_params
-    params.require(:presentation_content).permit(:page, :content, :text).merge(presentation_id: params[:presentation_id])
+    params.require(:content).permit(:page, :content, :text).merge(presentation_id: params[:presentation_id])
   end
+
 end
