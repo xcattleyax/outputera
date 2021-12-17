16c74606d45a722a1672fa630c03c7f22be6bd4e
xcattleyax <novels.satellite@gmail.com>
2021-12-17 14:22:35 +0900

    コントローラーcreateアクション設定

---
+++ app/controllers/note_answers_controller.rb
@@ -0,0 +1,6 @@
+class NoteAnswersController < ApplicationController
+
+  def create
+  end
+  
+end
--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,7 @@ Rails.application.routes.draw do
   resources :trends, except:[:index, :update]
   resources :notes, only:[:index, :new, :show, :create] do
     resources :note_comments, only:[:create, :show]
+    resources :note_answers, only:[:create]
   end
   resources :presentations, only:[:index, :new, :create, :show, :update] do
     resources :contents, only:[:new, :create, :edit, :update]
