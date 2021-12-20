e4caa44842d17be01ea1150167337845a393ea09
xcattleyax <novels.satellite@gmail.com>
2021-12-20 16:11:18 +0900

    投稿一覧ページの実装

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,11 @@
 class PostsController < ApplicationController
 
-  def index
+  def show
+    @user = User.find(params[:id])
+    @trends = @user.trends
+    @notes = @user.notes.order('created_at DESC')
+    presentations = @user.presentations.order('created_at DESC')
+    @presentations = presentations.where(detail_id: 2)
   end
-  
+
 end
