b5c2fd7ae0c34d449b21f13b1ddf3b7b6fa467c0
xcattleyax <novels.satellite@gmail.com>
2021-12-20 16:53:53 +0900

    解答ページへの他ユーザーの遷移制限の実装

--- app/controllers/note_comments_controller.rb
+++ app/controllers/note_comments_controller.rb
@@ -1,4 +1,6 @@
 class NoteCommentsController < ApplicationController
+  before_action :move_to_root, only:[:show]
+
   def create
     @comment = NoteComment.new(comment_params)
     if @comment.save
@@ -11,7 +13,6 @@ class NoteCommentsController < ApplicationController
 
   def show
     @comment = NoteComment.find(params[:id])
-    @note = Note.find(params[:note_id])
     @answer = NoteAnswer.new
   end
 
@@ -25,5 +26,12 @@ class NoteCommentsController < ApplicationController
 
   def comment_params
     params.require(:note_comment).permit(:comment).merge(user_id: current_user.id, note_id: params[:note_id])
+  end
+
+  def move_to_root
+    @note = Note.find(params[:note_id])
+    unless @note.user_id == current_user.id
+      redirect_to root_path
+    end
   end
 end
