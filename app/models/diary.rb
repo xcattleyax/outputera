91860dd998d61d67f8f926ad0bcd65e40020a1fa
xcattleyax <novels.satellite@gmail.com>
2021-12-21 15:12:07 +0900

    searchアクションの定義

--- app/models/diary.rb
+++ app/models/diary.rb
@@ -18,4 +18,30 @@ class Diary < ApplicationRecord
       'OK'
     end
   end
+
+  def self.note_search(category_id, title)
+    if category_id != "" && title != ""
+      notes = Note.where(category_id: category_id)
+      notes.where('title LIKE(?)', "%#{title}%")
+    elsif title != ""
+      Note.where('title LIKE(?)', "%#{title}%")
+    elsif category_id != ""
+      Note.where(category_id: category_id)
+    else
+      Note.all
+    end
+  end
+
+  def self.presentation_search(category_id, title)
+    if category_id != "" && title != ""
+      presentations = Presentation.where(category_id: category_id)
+      presentations.where('title LIKE(?)', "%#{title}%")
+    elsif title != ""
+      Presentation.where('title LIKE(?)', "%#{title}%")
+    elsif category_id != ""
+      Presentation.where(category_id: category_id)
+    else
+      Presentation.all
+    end
+  end
 end
