8b5082517b0e788c429e0c53f5957eedff0a75f6
xcattleyax <novels.satellite@gmail.com>
2021-12-21 11:05:33 +0900

    opinionモデル作成

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_12_20_022634) do
+ActiveRecord::Schema.define(version: 2021_12_21_020352) do
 
   create_table "contents", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.integer "page", null: false
@@ -61,6 +61,15 @@ ActiveRecord::Schema.define(version: 2021_12_20_022634) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.index ["user_id"], name: "index_notes_on_user_id"
+  end
+
+  create_table "opinions", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "title", null: false
+    t.text "content", null: false
+    t.bigint "user_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_opinions_on_user_id"
   end
 
   create_table "presentation_answers", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
@@ -120,6 +129,7 @@ ActiveRecord::Schema.define(version: 2021_12_20_022634) do
   add_foreign_key "note_comments", "notes"
   add_foreign_key "note_comments", "users"
   add_foreign_key "notes", "users"
+  add_foreign_key "opinions", "users"
   add_foreign_key "presentation_comments", "presentations"
   add_foreign_key "presentation_comments", "users"
   add_foreign_key "presentations", "users"
