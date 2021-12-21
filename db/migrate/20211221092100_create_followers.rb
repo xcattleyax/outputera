3508b7bfdc8e540cef315948bd2bc631345ae127
xcattleyax <novels.satellite@gmail.com>
2021-12-21 18:22:19 +0900

    モデルの作成

---
+++ db/migrate/20211221092100_create_followers.rb
@@ -0,0 +1,8 @@
+class CreateFollowers < ActiveRecord::Migration[6.0]
+  def change
+    create_table :followers do |t|
+      t.references :user, null: false, foreign_key: true
+      t.timestamps
+    end
+  end
+end
