9e9a9c08723f8640d601848e426322369c147a07
xcattleyax <novels.satellite@gmail.com>
2021-12-22 10:26:23 +0900

    followersテーブルへの保存実装

--- app/controllers/followers_controller.rb
+++ app/controllers/followers_controller.rb
@@ -1,5 +1,17 @@
 class FollowersController < ApplicationController
 
   def create
+    binding.pry
+    @follower = Follower.new(follower_params)
+    if @follower.save
+      redirect_to root_path
+    else
+      render "posts/show"
+    end
+  end
+
+  private
+  def follower_params
+    params.permit(:follow_id).merge(user_id: current_user.id)
   end
 end
